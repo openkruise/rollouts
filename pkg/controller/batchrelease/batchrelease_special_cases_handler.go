4de4b841579257ce714a8f221b62a560c19c86ab
veophi <vec.g.sun@gmail.com>
2022-02-16 16:20:32 +0800

    add license statement
    Signed-off-by: veophi <vec.g.sun@gmail.com>

--- pkg/controller/batchrelease/batchrelease_special_cases_handler.go
+++ pkg/controller/batchrelease/batchrelease_special_cases_handler.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package batchrelease
 
 import (
@@ -52,6 +68,12 @@ func (r *Executor) handleSpecialCases(controller workloads.WorkloadController) (
 		action = Keep
 		result = reconcile.Result{RequeueAfter: DefaultShortDuration}
 
+	case workloadEvent == workloads.WorkloadReplicasChanged:
+		reason = "ReplicasChanged"
+		message = "workload is scaling, paused and wait for it to be done"
+		needStopThisRound = true
+		action = Recalculate
+
 	case r.releasePlanPaused():
 		reason = "PlanPaused"
 		message = "release plan is paused, no need to reconcile"
@@ -79,12 +101,6 @@ func (r *Executor) handleSpecialCases(controller workloads.WorkloadController) (
 		message = "workload is table or rolling back, stop the release plan"
 		needStopThisRound = false
 		action = RollingBack
-
-	case workloadEvent == workloads.WorkloadReplicasChanged:
-		reason = "ReplicasChanged"
-		message = "workload is scaling, paused and wait for it to be done"
-		needStopThisRound = true
-		action = Recalculate
 
 	case workloadEvent == workloads.WorkloadPodTemplateChanged:
 		reason = "RevisionChanged"
