c2061b4a62c38ab1c1691049b17ec37fcb13ca53
yunbo <yunbo10124scut@gmail.com>
2024-06-05 09:53:24 +0800

    improve existing e2e logic to avoid unexpected behaviour
    Signed-off-by: yunbo <yunbo10124scut@gmail.com>

--- pkg/controller/rollout/rollout_canary.go
+++ pkg/controller/rollout/rollout_canary.go
@@ -210,6 +210,7 @@ func (m *canaryReleaseManager) doCanaryMetricsAnalysis(c *RolloutContext) (bool,
 
 func (m *canaryReleaseManager) doCanaryPaused(c *RolloutContext) (bool, error) {
 	if m.doCanaryJump(c) {
+		klog.Infof("rollout(%s/%s) canary step jumped", c.Rollout.Namespace, c.Rollout.Name)
 		return false, nil
 	}
 	canaryStatus := c.NewStatus.CanaryStatus
