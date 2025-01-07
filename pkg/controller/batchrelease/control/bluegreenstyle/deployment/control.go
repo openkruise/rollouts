a5a6642e9e2a51fc98f0505fe6024c6502afd88d
AiRanthem <zhongtianyun.zty@alibaba-inc.com>
2025-01-07 19:29:03 +0800

    Feature: support batch-id labeling for blue-green strategy
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- pkg/controller/batchrelease/control/bluegreenstyle/deployment/control.go
+++ pkg/controller/batchrelease/control/bluegreenstyle/deployment/control.go
@@ -214,6 +214,7 @@ func (rc *realController) CalculateBatchContext(release *v1beta1.BatchRelease) (
 		CurrentSurge:   currentSurge,
 		DesiredSurge:   desiredSurge,
 		UpdateRevision: release.Status.UpdateRevision,
+		StableRevision: release.Status.StableRevision,
 
 		Replicas:               rc.Replicas,
 		UpdatedReplicas:        rc.Status.UpdatedReplicas,
