1468fa0306e611e16d2514cc70565b25bed67b09
AiRanthem <zhongtianyun.zty@alibaba-inc.com>
2025-06-12 20:19:28 +0800

    Fix: Webhook will take over Deployment even Rollout CR doesn't exist
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- pkg/controller/rollout/rollout_controller.go
+++ pkg/controller/rollout/rollout_controller.go
@@ -86,15 +86,6 @@ type RolloutReconciler struct {
 // +kubebuilder:rbac:groups=apps.kruise.io,resources=daemonsets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=networking.istio.io,resources=virtualservices;destinationrules,verbs=get;list;watch;update;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Rollout object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *RolloutReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	// Fetch the Rollout instance
 	rollout := &v1beta1.Rollout{}
