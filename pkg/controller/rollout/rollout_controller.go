d34427f6770a69026a65eb71d0a0a16e35d42c92
Ai Ranthem <zhongtianyun.zty@alibaba-inc.com>
2025-06-20 17:33:24 +0800

    Fix: Webhook will take over Deployment even Rollout CR doesn't exist (#279)
    * Fix: Webhook will take over Deployment even Rollout CR doesn't exist (#278)
    
    (cherry picked from commit deaa5f38b579b4c1be2db8ed8b42cb7dbfcb52ce)
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>
    
    * Fix: Webhook will take over Deployment even Rollout CR doesn't exist (#278)
    
    (cherry picked from commit deaa5f38b579b4c1be2db8ed8b42cb7dbfcb52ce)
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>
    
    ---------
    
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- pkg/controller/rollout/rollout_controller.go
+++ pkg/controller/rollout/rollout_controller.go
@@ -84,15 +84,6 @@ type RolloutReconciler struct {
 // +kubebuilder:rbac:groups=apps.kruise.io,resources=daemonsets,verbs=get;list;watch;update;patch
 // +kubebuilder:rbac:groups=apps.kruise.io,resources=daemonsets/status,verbs=get;update;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Rollout object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *RolloutReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	// Fetch the Rollout instance
 	rollout := &v1alpha1.Rollout{}
